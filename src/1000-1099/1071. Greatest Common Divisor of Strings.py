78df2404b0abbd11871065e1a5821feb20f5dde8
Krylov Sergey <krylov.sergey.sergeevich@gmail.com>
2023-02-02 15:43:45 +0300

    Fix style and add docs

--- src/1000-1099/1071. Greatest Common Divisor of Strings.py
+++ src/1000-1099/1071. Greatest Common Divisor of Strings.py
@@ -67,6 +67,22 @@ class Solution:
             return ""
 
 
+"""
+GCD - Наибольший общий делитель строк.
+"""
+
+
+class BestSolution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if len(str1) < len(str2):
+            str1, str2 = str2, str1
+        if str1 == str2:
+            return str1
+        if str1[:len(str2)] != str2:
+            return ""
+        return self.gcdOfStrings(str1[len(str2):], str2)
+
+
 if __name__ == "__main__":
     solution = Solution()
 
